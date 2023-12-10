beebdddaa55d7ffa9bc5624d64d89c512a53f639
Otiong <oscarloltiong@gmail.com>
2023-12-10 03:56:39 -0800

    Imported text and got player health to work
    Player will lose health whenever it touches enemy and there's UI for it

---
+++ Hill Bill Thrill/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health = 10f;
+    public bool enemyHit = false;
+
+    
+    public Image healthBar;
+    public float damage;
+    // Start is called before the first frame update
+    [Header("Health Variables")]
+    public bool canDamage = true;
+    public bool isHit = false;
+    public bool hasSparked = false;
+    public float maxHealth;
+    public float damage_reduction = 10f;
+    public float totalDamage = 10f;
+    public float invul_dur = 2f;
+    public Vector2 offset;
+
+    [Header("Other File Includes")]
+    // public ParticleSystem spark;
+    public CameraFollow cameraShake;
+    public GameObject gameOver;
+    public static event Action OnPlayerDamaged;
+
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Time.timeScale = 0f;
+            gameOver.SetActive(true);
+        }
+    }
+
+    public void Restart()
+    {
+        gameOver.SetActive(false);
+        health = 10f;
+        healthBar.fillAmount = 10f;
+        Time.timeScale = 1f;
+    }
+
+    public IEnumerator Damaged()
+    {
+        canDamage = false;
+        health -= damage;
+        healthBar.fillAmount = health / 20f;
+    
+        yield return new WaitForSeconds(0.15f);
+        canDamage = true;
+    }
+
+    public void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.tag == "Enemy") 
+        {
+            if (health > 0) health -= 2f * Time.deltaTime;
+            healthBar.fillAmount = health / 20f;
+            Debug.Log("Hello???");
+        }
+    }
+
+    public void TakeDamage(float amount){
+        health -= amount;
+        OnPlayerDamaged?.Invoke();
+    }
+}
